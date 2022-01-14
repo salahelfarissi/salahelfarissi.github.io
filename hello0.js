ac86d0579920f66026c5f14391af09eea073910e
SalahEddine El Farissi <salaheddineelfarissi@gmail.com>
2022-01-14 11:10:31 +0100

    Reorganize structures

---
+++ hello0.js
@@ -0,0 +1,4 @@
+const name = prompt('What is your name?');
+const age = prompt('How old are you?');
+const height = prompt('How tall are you?');
+alert(`${name} is ${age} years old and ${height} tall.`);
