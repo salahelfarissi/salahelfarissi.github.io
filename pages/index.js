728ca2d7758ccbd0ab535b69164f6ade7afb954e
salahelfarissi <salaheddine.elfarissi@gmail.com>
2023-02-23 10:33:12 +0100

    Delete footer

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date";
-import Footer from "../components/shared/Footer";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -49,7 +48,6 @@ export default function Home({ allPostsData }) {
           </ul>
         </section>
       </Layout>
-      {/* <Footer /> */}
     </>
   );
 }
